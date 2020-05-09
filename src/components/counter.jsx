6c9d4aabe5940f370fb8868a6c9a9b4d4bba0785
mamatha <mamathagowdg@gmail.com>
2020-05-09 10:16:57 +0530

    add

---
+++ src/components/counter.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: this.props.counter.value,
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+  handleIncrement = (product) => {
+    //this.state.count++;
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+    //console.log("increment clicked", this);
+  };
+
+  render() {
+    console.log("props", this.props);
+    return (
+      <div>
+        {/* <h4>{this.props.id}</h4> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(1)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+export default Counter;
